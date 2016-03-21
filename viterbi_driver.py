f591a1c52a4951989fa2c731ff70f22922350799
leslie m <ljm391@nyu.edu>
2016-03-21 14:07:50 -0400

    Likelihood

--- viterbi_driver.py
+++ viterbi_driver.py
@@ -8,6 +8,7 @@ training = "WSJ_POS_CORPUS_FOR_STUDENTS/WSJ_02-21.pos"
 corpus_list = viterbi.corpus_list(training)
 #print("THE NUMBER OF SENTENCES",len(corpus_list)) 
 corpus_dictionary = viterbi.corpus_dictionary(training) 
+keys = viterbi.key_list(corpus_dictionary) 
 #print(corpus)
  
 #print(corpus_dictionary)
@@ -17,6 +18,12 @@ corpus_dictionary = viterbi.corpus_dictionary(training)
 
 transition_table = viterbi.transition_table(corpus_dictionary,corpus_list) 
 #print(transition_table) 
+
+#likelihood table 
+#print(keys)
+word_dic = viterbi.word_dic(corpus_list,keys) 
+word_freq = viterbi.word_freq(corpus_dictionary,word_dic) 
+#print(word_dic)
 print("done")
 
 
